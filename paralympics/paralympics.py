d6f9119b394c41439376ecd544fef899170d3e5f
Y_M <mengyang011002@outlook.com>
2024-02-09 17:36:48 +0000

    1

--- paralympics/paralympics.py
+++ paralympics/paralympics.py
@@ -1,10 +1,17 @@
-from flask import Flask
-from markupsafe import escape
+# from flask import Flask
+# from markupsafe import escape
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# @app.route("/<name>")
+# def hello(name=None):
+#     return f"Hello, {escape(name)}!"
+
+from flask import current_app as app
 
 
 @app.route('/')
-@app.route("/<name>")
-def hello(name=None):
-    return f"Hello, {escape(name)}!"
+def hello():
+  return f"Hello!"
